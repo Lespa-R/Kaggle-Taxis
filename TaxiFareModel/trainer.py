7c03cfee6bd1c5d9dabb6e75bc576f81ecdbb227
Robin Lespagnol <lespagnol.robin@gmail.com>
2021-11-16 17:03:18 +0100

    Ajout du lien MLflow et premier test fonctionnel

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -8,8 +8,15 @@ from TaxiFareModel.utils import compute_rmse
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import mlflow
+from mlflow.tracking import MlflowClient
+import joblib
+from memoized_property import memoized_property
+
 
 class Trainer():
+    MLFLOW_URI = "https://mlflow.lewagon.co/"
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -18,6 +25,29 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(self.MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -37,13 +67,16 @@ class Trainer():
 
     def run(self):
         """set and train the pipeline"""
+        self.experiment_name = "[Lyon-City] - Lespa - V1"
         self.set_pipeline()
         self.pipeline.fit(self.X, self.y)
+        self.mlflow_log_param("linear_model", "Linear")
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric("rmse", rmse)
         return rmse
 
 
